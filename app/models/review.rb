52a5f6b967dc2d2f74c3f02eb83e7e9b0f4933c4
shakar hassan <shakarmohammed2002@gmail.com>
2023-09-18 11:03:51 +0200

    done

---
+++ app/models/review.rb
@@ -0,0 +1,4 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+validates :rating, presence: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+Restaurant.create(name: "fun's restaurant", address: "rudy_str" , phone_number: "017236634798759", category: "chinese", rating: 5)
+Restaurant.create(name: "ardin restaurant", address: "Alexander plazt" , phone_number: "01727686579", category: "belgian", rating: 5)
+Restaurant.create(name: "akane's restaurant", address: "postdama plazt" , phone_number: "01778468266523", category: "japanese", rating: 5)
+Restaurant.create(name: "james's restaurant", address: "spandau" , phone_number: "0172366347987654", category: "french", rating: 5)
+Restaurant.create(name: "matteo's", address: "somewhere" , phone_number: "01723668765", category: "italian", rating: 5)
